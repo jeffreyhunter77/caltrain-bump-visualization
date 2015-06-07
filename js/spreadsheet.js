10d7770766e718ef89357bbb681b61a50072d1aa
Jeff Hunter <jeff@missioncriticallabs.com>
2015-06-07 12:14:43 -0700

    Add ability to produce bump data from google spreadsheet data

--- js/spreadsheet.js
+++ js/spreadsheet.js
@@ -1,3 +1,94 @@
-function rows(data) {
-  return data.feed.entry;
+/**
+ * A class for working with bump data in a Google spreadsheet
+ */
+function BumpSpreadSheet(data) {
+  this.data = data;
 }
+
+$.extend(BumpSpreadSheet.prototype, {
+  
+  /** Return the set of row data */
+  rows: function() {
+    return this.data.feed.entry;
+  },
+  
+  /** The key used in the data for a given heading */
+  _cellKey: function(heading) {
+    return 'gsx$' + String(heading).replace(/[^A-Za-z0-9]+/g, '').toLowerCase();
+  },
+  
+  /** Return the data from a given cell address */
+  cell: function(rowIdx, heading) {
+    var row = this.rows()[rowIdx];
+    
+    if (!row) return;
+    
+    var cell = row[this._cellKey(heading)];
+    
+    return cell ? cell['$t'] : cell;
+  },
+  
+  /** Determine if a report was likely tweeted */
+  _wasTweeted: function(count, comments) {
+    return ( (parseInt(count) == 1) &&
+             (/^CAUTION: There may have been more than one bump! Transferred from Twitter/.test(comments)) );
+  },
+  
+  /** Add a tweeted bump to the count */
+  _addTweet: function(data) {
+    if (!data.tweeted) data.tweeted = 0;
+    ++data.tweeted;
+  },
+  
+  /** Add a regular bump to the count */
+  _addRegular: function(data, count) {
+    data.bumps = Math.max(data.bumps ? data.bumps : 0, count);
+  },
+  
+  /** Consolidate tweeted and regular bump reports */
+  _consolidateBumps: function(bumps) {
+    $.each(bumps, function(station, trains) {
+      $.each(trains, function(train, dates) {
+        $.each(Object.keys(dates), function(idx, date) {
+          var bumps = dates[date];
+          dates[date] = Math.max(bumps.bumps ? bumps.bumps : 0,
+                                 bumps.tweeted ? bumps.tweeted : 0);
+        });
+      });
+    });
+    
+    return bumps;
+  },
+  
+  /** Return the summarized bump data from the spreadsheet */
+  bumpData: function() {
+    
+    var bumps = {};
+    var rowCount = this.rows().length;
+    
+    for (var i = 0; i < rowCount; ++i) {
+      
+      var date     = this.cell(i, 'Date of bicycle bump(s)');
+      var count    = this.cell(i, 'Total number of bumped bikes');
+      var station  = this.cell(i, 'Departure station');
+      var train    = this.cell(i, 'Train number');
+      var comments = this.cell(i, 'Comments');
+      
+      if (String(train).trim() != '') {
+        if (!bumps[station]) bumps[station] = {};
+        if (!bumps[station][train]) bumps[station][train] = {};
+        if (!bumps[station][train][date]) bumps[station][train][date] = {};
+        
+        if (this._wasTweeted(count, comments))
+          this._addTweet(bumps[station][train][date]);
+        else
+          this._addRegular(bumps[station][train][date], parseInt(count));
+      }
+      
+    }
+    
+    return this._consolidateBumps(bumps);
+    
+  }
+  
+});
--- spec/spreadsheet_spec.js
+++ spec/spreadsheet_spec.js
@@ -1,15 +1,62 @@
 describe("process bumps", function() {
+  
+  beforeEach(function() {
+    sheet = new BumpSpreadSheet(testSSData);
+  });
 
   describe("rows", function() {
     
     it("should return all the rows from the spreadsheet data", function() {
-      expect(rows(testSSData).length).toBe(17);
+      expect(sheet.rows().length).toBe(18);
     });
     
-    it("should return the first row of data as the first element", function() {
-      expect(rows(testSSData)[0]['gsx$trainnumber']['$t']).toBe('314');
+  });
+  
+  describe("cell", function() {
+    
+    it("should return undefined for a row not in the sheet", function() {
+      expect(sheet.cell(200, 'Train number')).toBe(undefined);
+    });
+
+    it("should return undefined for a column not in the sheet", function() {
+      expect(sheet.cell(0, 'Nrain tumber')).toBe(undefined);
+    });
+
+    it("should return the data of the requested row and column", function() {
+      expect(sheet.cell(0, 'Train number')).toBe('314');
     });
     
+  });
+  
+  describe("bumpData", function() {
+    
+    it("should provide the total number of bumped bikes for a time and location", function() {
+      expect(sheet.bumpData()['22nd Street']['314']['05/04/2015']).toBe(3);
+    });
+    
+    it("should use the maximum number of bumps specified", function() {
+      expect(sheet.bumpData()['22nd Street']['314']['05/05/2015']).toBe(4);
+    });
+
+    it("should sum tweeted bumps for the same time and location", function() {
+      expect(sheet.bumpData()['22nd Street']['314']['05/06/2015']).toBe(3);
+    });
+
+    it("should the maximum of non-tweeted and tweeted bumps", function() {
+      expect(sheet.bumpData()['22nd Street']['314']['05/07/2015']).toBe(3);
+      expect(sheet.bumpData()['22nd Street']['314']['05/11/2015']).toBe(4);
+    });
+
+    it("should have separate information for each station and train", function() {
+      expect(sheet.bumpData()['22nd Street']['314']['05/13/2015']).toBe(2);
+      expect(sheet.bumpData()['22nd Street']['324']['05/13/2015']).toBe(3);
+      expect(sheet.bumpData()['Millbrae']['324']['05/13/2015']).toBe(4);
+    });
+
+    it("should ignore entries without a train number", function() {
+      expect(sheet.bumpData()['Redwood City']).toBe(undefined);
+    });
+
   });
 
 });
